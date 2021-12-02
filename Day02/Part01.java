4d632a04d162a8e2ef5506eb32214dc416a6a95b
Mattia Oldani <mattiaoldani6@gmail.com>
2021-12-02 08:01:04 +0100

    Day-02

---
+++ Day02/Part01.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Part01 {
+    static int Solve(List<String> input) {
+        int depth = 0, forward = 0;
+
+        for (String x : input) {
+            String[] cmd = x.split(" ");
+            if (cmd[0].equals("forward"))
+                forward += Integer.parseInt(cmd[1]);
+            else if (cmd[0].equals("down"))
+                depth += Integer.parseInt(cmd[1]);
+            else
+                depth -= Integer.parseInt(cmd[1]);
+        }
+
+        return depth * forward;
+    }
+}
---
+++ Day02/Part02.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Part02 {
+    static int Solve(List<String> input) {
+        int depth = 0, forward = 0, aim = 0;
+
+        for (String x : input) {
+            String[] cmd = x.split(" ");
+            if (cmd[0].equals("forward")) {
+                forward += Integer.parseInt(cmd[1]);
+                depth += (aim * Integer.parseInt(cmd[1]));
+            }
+            else if (cmd[0].equals("down"))
+                aim += Integer.parseInt(cmd[1]);
+            else
+                aim -= Integer.parseInt(cmd[1]);
+        }
+
+        return depth * forward;
+    }
+}
